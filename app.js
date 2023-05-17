8d2d01f5c88c31e2e609bd5ecff8323faffe2ff6
Hasibullah Aman <hasib.aman7@gmail.com>
2023-05-17 10:08:19 +0430

    add

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
+require("dotenv").config(); // load env file
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const expHbs = require('express3-handlebars');
+var bodyParser = require('body-parser')
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const mongoose = require('mongoose');
+const async = require("async");
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+/**
+ * Make MongoDB connection
+ */
+(async () => {
+    await mongoose.connect('mongodb://localhost:27017/lms');
+})();
+
+app.engine('hbs', expHbs({defaultLayout: 'layout', extname: '.hbs'}))
+app.set('view engine', 'hbs')
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// parse application/json
+app.use(bodyParser.json());
+
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
