46b9e7c52d630fae2d854a07ac59c65ab1e0b037
Hasibullah Aman <hasib.aman7@gmail.com>
2023-05-17 12:17:22 +0430

    delete view link in app.js

--- app.js
+++ app.js
@@ -5,13 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const expHbs = requier('express3-handlebars');
 var indexRouter = require('./routes/index');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
